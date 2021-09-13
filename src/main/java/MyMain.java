5b0822b4d8cb4f72cab575dda4a6da3bf177a1bb
lucass <24lspeier@athenian.org>
2021-09-13 14:04:52 -0700

    wdiuhjdfgdfjndfdfjkl

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,7 +5,8 @@ public class MyMain {
     //     divisibleByFour(5) -> false
     public static boolean divisibleByFour(int num) {
         // REPLACE WITH YOUR CODE HERE
-        return false;
+
+        return num % 4 == 0;
     }
 
     // This method prints every number from 1 up to and including the input biggest
@@ -15,14 +16,33 @@ public class MyMain {
     // ...
     public static void printNumsUpTo(int biggest) {
         // REPLACE THIS WITH YOUR CODE
+        String line = "";
+        int LineStuff = 0;
+        for (int i = 1; i <= biggest; i++) {
+            if (LineStuff == 4) {
+                System.out.println(line+" "+i);
+                line = "";
+                LineStuff = 0;
+            } else {
+                LineStuff += 1;
+                line = line + " " + i;
+            }
+        }
     }
 
     // This method is given an input x, and it adds up the sum of its digits
     // Ex. sumDigits(61) -> 7 because 6 + 1 = 7
     //     sumDigits(123) -> 6 because 1 + 2 + 3 = 6
     public static int sumDigits(int x) {
+        int l = (String.valueOf(x)).length();
+        int a = 0;
+        int lp = x;
+        for (int i = 10; i <= x*10;i*= 10) {
+            a = a + (lp % 10);
+            lp = lp/10;
+        }
+        return a;
         // REPLACE THIS WITH YOUR CODE
-        return 0;
     }
 
     // Bonus: This method returns true if the input num is a "self-divisor",
@@ -36,7 +56,14 @@ public class MyMain {
     // digit is NOT a self-divisor.
     public static boolean isSelfDivisor(int num) {
         // REPLACE THIS WITH YOUR CODE
-        return false;
+        int lp = num;
+        for (int i = 10; i <= num*10;i*= 10) {
+            if ((lp % 10) == 0 || num % (lp % 10) != 0) {
+                return false;
+            }
+            lp = lp/10;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -55,6 +82,7 @@ public class MyMain {
         // 16 17 18 19 20
 
         System.out.println();
+        sumDigits(61);
         System.out.println("sumDigits Tests:");
         System.out.println(sumDigits(61)); // 7 because 6 + 1 = 7
         System.out.println(sumDigits(123)); // 6 because 1 + 2 + 3 = 6
